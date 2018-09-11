70405617628582a23e1e9a0becdbefa2933bec60
haochengz <haochengzhao@outlook.com>
2018-09-11 16:38:45 +0800

    chores: rewrite pm2 deploy script

---
+++ ecosystem.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    {
+      name      : 'GEAS',
+      script    : 'server/entrance/app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : '47.106.125.144',
+      ref  : 'origin/master',
+      repo : 'git@github.com:haochengz/geas.git',
+      path : '/www/geas/production',
+      'post-deploy' : 'cnpm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'root',
+      host : '47.106.125.144',
+      ref  : 'origin/master',
+      repo : 'git@github.com:haochengz/geas.git',
+      path : '/www/geas/development',
+      'post-deploy' : 'cnpm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+}
