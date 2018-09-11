f7eb9ecf3fc4321b25da32b49e7b2f5902dec1e4
haochengz <haochengzhao@outlook.com>
2018-09-11 16:58:07 +0800

    chores: fix a bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     {
       name      : 'GEAS',
-      script    : 'server/entrance/app.js',
+      script    : 'server/entrance/start.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
